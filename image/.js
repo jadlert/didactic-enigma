82865d57c7d074e5437e968408c9b8be91dd30c9
Jadler Timak <jadlertelemaque@MacBook-Air.lan>
2023-03-24 12:47:51 -0400

    first commit

---
+++ image/.js
@@ -0,0 +1,7 @@
+<script>
+// Add a click event listener to the "Follow me on social media" button
+document.querySelector('.follow-btn').addEventListener('click', function() {
+	// Replace the URL below with your artist's social media profile URL
+	window.open('https://www.instagram.com/chrystelhajp/');
+});
+</script>
